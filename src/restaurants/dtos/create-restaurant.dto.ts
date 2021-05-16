a495920b04c130b4d36dcdfa8aec37143c253971
Miju Park <mijupark@Mijuui-MacBookPro.local>
2021-05-16 22:28:22 +0900

    [GraphQL] validating ArgsTypes

--- src/restaurants/dtos/create-restaurant.dto.ts
+++ src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,4 +1,5 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 /**
  * InputType : object 형태로 @Args에 전달되어야 하고
@@ -7,11 +8,19 @@ import { ArgsType, Field } from '@nestjs/graphql';
 @ArgsType()
 export class CreateRestaurantDto {
   @Field((is) => String)
+  @IsString()
+  @Length(5, 10)
   name: string;
+
   @Field((is) => Boolean)
+  @IsBoolean()
   isVegan: boolean;
+
   @Field((is) => String)
+  @IsString()
   address: string;
+
   @Field((is) => String)
+  @IsString()
   ownerName: string;
 }
