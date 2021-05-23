52a2b10a0b1f4cb080670d1d487e710e844bf3b0
Miju Park <mijupark@Mijuui-MacBookPro.local>
2021-05-23 14:20:05 +0900

    feat:[Database] TypeORM setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
@@ -8,11 +9,21 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
    * forRoot : GraphQL Module을 Root로 설정
    */
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'mijupark',
+      password: '12345',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       /**
        * Schema 파일 정의없이 Code로 Schema 자동 정의
        */
-      autoSchemaFile: true,//파일로 저장하지 않는다는 뜻
+      autoSchemaFile: true, //파일로 저장하지 않는다는 뜻
     }),
     RestaurantsModule,
   ],
