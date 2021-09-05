efb42d0fb937de3d1e6f1fb5bec5e37eeb11af2b
Miju Park <mijupark@Mijuui-MacBookPro.local>
2021-09-05 17:44:24 +0900

    feat: Mail module

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,16 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { CONFIG_OPTIONS } from './jwt.constants';
 import { JwtModuleOptions } from './jwt.interfaces';
 import * as jwt from 'jsonwebtoken';
+import { CONFIG_OPTIONS } from 'src/common/common.constants';
 
 @Injectable()
 export class JwtService {
   constructor(
     @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
   ) {}
-  hello() {
-    console.log('hello');
-  }
   sign(payload: object): string {
     return jwt.sign(payload, this.options.privateKey);
   }
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,21 +1,19 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
-import { JwtModuleOptions } from './jwt.interfaces';
-import { JwtService } from './jwt.service';
+
+import { MailModuleOptions } from './mail.interfaces';
 
 @Module({})
 @Global()
-export class JwtModule {
-  static forRoot(options: JwtModuleOptions): DynamicModule {
+export class MailModule {
+  static forRoot(options: MailModuleOptions): DynamicModule {
     return {
-      module: JwtModule,
-      exports: [JwtService],
+      module: MailModule,
       providers: [
         {
           provide: CONFIG_OPTIONS,
           useValue: options,
         },
-        JwtService,
       ],
     };
   }
